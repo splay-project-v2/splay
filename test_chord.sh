fbf5ef178f70b76f581244ec74d28d9d8036f643
Willfrit <remy-voet@outlook.fr>
2018-07-11 14:55:00 +0200

    resolve merge

--- test_chord.sh
+++ test_chord.sh
@@ -2,8 +2,4 @@
 ./rebuild.sh
 # gnome-terminal -e "docker-compose logs -f daemon" &
 # gnome-terminal -e "docker-compose logs -f" &
-<<<<<<< HEAD
 docker run --network splay_default -it splay_terminal /bin/bash -c "./splay-start-session.lua; sleep 5s; ./splay-submit-job.lua -n 2 app/cyclon.lua"
-=======
-docker run --network splay_default -it splay_terminal /bin/bash -c "./splay-start-session.lua; sleep 5s; ./splay-submit-job.lua -n 1 app/cyclon.lua"
->>>>>>> 971fb621aa367f893abbe06a33b5280950c6f5ee
