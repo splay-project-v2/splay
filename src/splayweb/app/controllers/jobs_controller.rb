30397af14f4b7e2a9d70756ea8857cd4200d603d
Samuel Monroe <spat.monroe@gmail.com>
2018-07-12 15:08:18 +0200

    [FEATURE] Job can be killed on SplayWeb

--- src/splayweb/app/controllers/jobs_controller.rb
+++ src/splayweb/app/controllers/jobs_controller.rb
@@ -2,4 +2,14 @@ class JobsController < ApplicationController
   def index
     @jobs = Job.all
   end
+
+  def destroy
+    job = Job.find(params[:id])
+    if job.update(command: 'KILL')
+      flash[:notice] = 'Job was killed.'
+    else
+      flash[:notice] = 'Problem killing job.'
+    end
+    redirect_to action: 'index'
+  end
 end
--- src/splayweb/config/routes.rb
+++ src/splayweb/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :jobs, only: [:index]
+  resources :jobs, only: [:index, :destroy]
   resources :splayds, only: [:index]
 end
