81531bb72d56fba28572d1d2426a56768175bc92
Samuel Monroe <spat.monroe@gmail.com>
2018-07-12 13:53:30 +0200

    Prefered Controller is now the forking one
    Better performance is achieved by doing so

--- src/controller/controller.rb
+++ src/controller/controller.rb
@@ -21,17 +21,19 @@
 ## You should have received a copy of the GNU General Public License
 ## along with Splayd. If not, see <http://www.gnu.org/licenses/>.
 
-
-require './lib/common'
-
-# We force only one log daemon and one splayd daemon
-SplayControllerConfig::NumLogd = 1
-SplayControllerConfig::NumSplayd = 1
+# This file will execute all the daemons (including multiple splayds and logds) on
+# this computer. Because DBI is not fork safe (if a process halt, the connection
+# is closed), we give a different DB connection to every process. We do that too
+# because ruby (1.8) threads are not efficient and a splayd (daemon) will have
+# dificulties to manage more than 100 threads (=> splayds).
+#
+# Each 'daemon' can be run standalone. Following the example in this file, you can
+# see how easily you would be able to run multiple splayd/jobs on multiple
+# computers.
 
 require './lib/all'
 
 $log.level = Logger::DEBUG
-#$log.level = Logger::INFO
 
 puts
 puts ">>> Splayd Controller #{SplayControllerConfig::CTLVersion} <<<"
@@ -41,22 +43,56 @@ puts "Splay Controller is licensed under GPL v3, see COPYING"
 puts
 
 Splayd.init
-$db["UPDATE locks SET job_reservation='0' WHERE id ='1'"]
+$db.run("UPDATE locks SET job_reservation='0' WHERE id ='1'")
 
-# Daemons
-LogdServer.new.run
-SplaydServer.new.run
-JobdStandard.run
+$db.disconnect
+$dbt.disconnect
 
-JobdTrace.init
-JobdTrace.run
+for i in (0...SplayControllerConfig::NumLogd)
+	fork do
+		$db = DBUtils.get_new
+		LogdServer.new(SplayControllerConfig::LogdPort + i).run.join
+	end
+end
 
-Unseend.run
-Statusd.run
-Loadavgd.run
-Blacklistd.run
+for i in (0...SplayControllerConfig::NumSplayd)
+	fork do
+		$db = DBUtils.get_new
+		$dbt = DBUtils.get_new
+		SplaydServer.new(SplayControllerConfig::SplaydPort + i).run.join
+	end
+end
 
-#Splayd.localize_all
+fork do
+	$db = DBUtils.get_new
+	JobdStandard.run.join
+end
+
+fork do
+	$db = DBUtils.get_new
+	JobdTrace.init
+	JobdTrace.run.join
+end
+
+fork do
+	$db = DBUtils.get_new
+	Unseend.run.join
+end
+
+fork do
+	$db = DBUtils.get_new
+	Statusd.run.join
+end
+
+fork do
+	$db = DBUtils.get_new
+	Loadavgd.run.join
+end
+
+fork do
+	$db = DBUtils.get_new
+	Blacklistd.run.join
+end
 
 # end only on interruption
 loop do sleep 1000 end
