24fda24d4180db009e5858c8a82da3e2774957ed
Samuel Monroe <spat.monroe@gmail.com>
2018-07-13 13:58:47 +0200

    Registration works on SplayWeb

--- src/splayweb/config/routes.rb
+++ src/splayweb/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
+  resources :home, only: [:index]
   resources :jobs, only: [:index, :destroy]
   resources :splayds, only: [:index, :show, :destroy]
   resources :users, only: [:new, :create]
