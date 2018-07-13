3475352579ed578be0c4337df0d2c4c58be35682
Willfrit <remy-voet@outlook.fr>
2018-07-13 15:27:00 +0200

    test_cyclon fix

--- test_cyclon.sh
+++ test_cyclon.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 ./rebuild.sh
-docker run -it splay_cli_client /bin/bash -c "./splay-start-session.lua; sleep 1s; ./splay-submit-job.lua -n 5 app/cyclon.lua"
+docker run  --network splay_default -it splay_terminal /bin/bash -c "./splay-start-session.lua; sleep 1s; ./splay-submit-job.lua -n 5 app/cyclon.lua"
