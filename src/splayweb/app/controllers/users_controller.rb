cc0a881ab65507061fd30c6226d3de97e98f9c8e
Samuel Monroe <spat.monroe@gmail.com>
2018-07-13 11:44:36 +0200

    User can register on SplayWeb

--- src/splayweb/app/controllers/users_controller.rb
+++ src/splayweb/app/controllers/users_controller.rb
@@ -4,6 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'Account is created!'
+      redirect_to controller: 'home', method: 'get'
+    else
+      flash[:alert] = 'An error occured'
+      render 'new'
+    end
+  end
 
+  private
+  def user_params
+    params.require(:user).permit(:login, :email, :password, :password_confirmation)
   end
 end
