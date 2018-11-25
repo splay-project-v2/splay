9fd4116e5b78e77ffbe6db016bc9e3bc1d77715d
Samuel Monroe <spat.monroe@gmail.com>
2018-11-25 21:05:38 +0900

    Small changes according to new naming conventions

--- rebuild.sh
+++ rebuild.sh
@@ -3,7 +3,7 @@ docker-compose kill
 docker-compose rm -f
 docker kill $(docker ps -q)
 docker rm $(docker ps -a -q)
-docker-compose build 
-docker-compose up -d cli_server
+docker-compose build
+docker-compose up -d backend
 docker-compose scale daemon=5
 #docker-compose up -d splayweb
